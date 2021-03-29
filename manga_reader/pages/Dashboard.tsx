e0d0a2e5eed90d4ba6c13be99befb7ef2d53095c
JustDancePC <jesusdavid268@outlook.com>
2021-03-29 14:49:48 -0400

    searchbar icon change

--- manga_reader/pages/Dashboard.tsx
+++ manga_reader/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {View, TextStyle, Pressable} from 'react-native';
-import {Input, Card, Icon} from 'react-native-elements';
+import {TextStyle, Pressable} from 'react-native';
+import {Input, Icon} from 'react-native-elements';
 import {ScrollView} from 'react-native-gesture-handler';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
@@ -12,6 +12,7 @@ import {CompositeNavigationProp} from '@react-navigation/native';
 import {connect, ConnectedProps} from 'react-redux';
 
 import type {RootReducerType as CombinedState} from '../store/rootReducer';
+import {UserAvatar} from '../components';
 
 type DashboardScreenNavigationProp = CompositeNavigationProp<
   BottomTabNavigationProp<RootTabNavigatorParamList, 'Dashboard'>,
@@ -33,53 +34,38 @@ type Props = PropsFromRedux & {
 const Dashboard = (props: Props) => {
   return (
     <ScrollView>
-      <View>
-        <Pressable
-          android_ripple={{
-            borderless: false,
-            color: 'gray',
-          }}
-          onPress={() =>
-            props.navigation.navigate('ProfileModal', {
-              deviceUser: true,
-              name: props.state.name,
-              user_id: props.state.id,
-            })
-          }>
-          <Icon
-            style={textStyle[1]}
-            name={'user'}
-            color={'black'}
-            type={'font-awesome-5'}
-            size={40}
-          />
-        </Pressable>
-
-        <View style={textStyle[0]}>
-          <Input placeholder={'Search.....'} />
-        </View>
-
-        <Card.Divider style={{top: '-50%', backgroundColor: 'black'}} />
-      </View>
+      <Input
+        placeholder={'Search something'}
+        style={textStyle[0]}
+        leftIcon={
+          <Pressable
+            android_ripple={{
+              color: 'gray',
+              borderless: true,
+            }}
+            onPress={() =>
+              props.navigation.navigate('ProfileModal', {
+                deviceUser: true,
+                name: props.state.name,
+                user_id: props.state.id,
+              })
+            }>
+            <UserAvatar
+              size={'medium'}
+              user_id={props.state.id}
+              style={undefined}
+            />
+          </Pressable>
+        }
+      />
     </ScrollView>
   );
 };
 
 const textStyle: TextStyle[] = [
   {
-    color: 'black',
-    alignSelf: 'center',
-    fontSize: 20,
-    width: 290,
-    top: -110,
-  },
-  {
-    color: 'black',
-    alignSelf: 'flex-start',
-    paddingTop: 30,
-    paddingBottom: 50,
-    left: '2%',
-    top: '-1.5%',
+    fontSize: 18,
+    marginTop: 15,
   },
 ];
 
