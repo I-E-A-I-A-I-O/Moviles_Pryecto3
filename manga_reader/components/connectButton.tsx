a3214af80bf504f2a716fb0bdfc39dac72ffa251
JustDancePC <jesusdavid268@outlook.com>
2021-03-31 09:14:28 -0400

    lo que llevo

---
+++ manga_reader/components/connectButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Button} from 'react-native-elements';
+
+type Props = {
+    user_id: string;
+    token: string;
+    session: string;
+}
+
+class ConnectButton() {
+
+}
+
+export default ConnectButton;
