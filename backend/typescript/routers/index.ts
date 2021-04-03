ccf28d42e71d84117018c98ad949f9262897198a
Anderson Gutierrez <andersonpantor@gmail.com>
2021-04-03 00:28:24 -0400

    Rutas para los post

--- backend/typescript/routers/index.ts
+++ backend/typescript/routers/index.ts
@@ -2,5 +2,6 @@ import {router as userRouter} from './user_routes';
 import {router as codeRouter} from './code_routes';
 import {router as searchBarRouter} from './search';
 import {router as connectsRouter} from './connections_router';
+import {router as postRouters} from './post_router'
 
-export {userRouter, codeRouter, searchBarRouter, connectsRouter};
+export {userRouter, codeRouter, searchBarRouter, connectsRouter, postRouters};
