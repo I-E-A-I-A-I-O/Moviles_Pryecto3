8035ca0626990adacc61fe1cbb943ea79825e61f
Anderson Gutierrez <andersonpantor@gmail.com>
2021-04-11 22:36:28 -0400

    Agregando Avatar y Nombre  de usuario

--- manga_reader/pages/RootTabPages/pageToPost.tsx
+++ manga_reader/pages/RootTabPages/pageToPost.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import {ScrollView, View, ViewStyle} from 'react-native';
-import {Text} from 'react-native-elements';
-import {PostMaker} from '../../components';
+import {View, ViewStyle, ScrollView, Text} from 'react-native';
+import {PostMaker, UserAvatar} from '../../components';
+import type {RootReducerType as CombinedState} from '../../store/rootReducer';
+import {connect, ConnectedProps} from 'react-redux';
 
-const pageToPost = () => {
+const mapStateToProps = (state: CombinedState) => ({
+  state: state.session,
+});
+
+const connector = connect(mapStateToProps, {});
+
+type Props = ConnectedProps<typeof connector>;
+
+const pageToPost = (props: Props) => {
   return (
-    <View style={style}>
-      <PostMaker />
-    </View>
+    <ScrollView>
+      <View>
+        <UserAvatar 
+          user_id={props.state.id}
+          size={'large'}
+          style={{left: 25, top: 30}}
+        />
+        <Text style={{fontSize: 30, top: -28 ,left: 130}} >{props.state.name}</Text>
+      </View>
+      <View style={style}>
+        <PostMaker />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -15,4 +34,4 @@ const style: ViewStyle = {
   flex: 1,
 };
 
-export default pageToPost;
+export default connector(pageToPost);
