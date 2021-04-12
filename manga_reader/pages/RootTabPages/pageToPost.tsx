17219bd81cb7033990ff28b60979ce428e6a7ac9
Anderson Gutierrez <andersonpantor@gmail.com>
2021-04-12 00:40:37 -0400

    Agregando Avatar y nombre de usuario al pagina de creacion de post

--- manga_reader/pages/RootTabPages/pageToPost.tsx
+++ manga_reader/pages/RootTabPages/pageToPost.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import {ScrollView, View, ViewStyle} from 'react-native';
 import {Text} from 'react-native-elements';
-import {PostMaker} from '../../components';
+import {PostMaker, UserAvatar} from '../../components';
+import type {RootReducerType as CombinedState} from '../../store/rootReducer';
+import {connect, ConnectedProps} from 'react-redux';
 
-const pageToPost = () => {
+const mapStateToProps = (state: CombinedState) => ({
+  state: state.session,
+});
+
+const connector = connect(mapStateToProps, {});
+
+type Props = ConnectedProps<typeof connector>;
+
+const pageToPost = (props: Props) => {
   return (
-    <View style={style}>
-      <PostMaker />
-    </View>
+    <ScrollView>
+      <View>
+        <UserAvatar 
+          user_id={props.state.id}
+          size={'large'}
+          style={{left: 17, top: 30}}
+        />
+        <Text style={{fontSize: 30, top: -28 ,left: 110}} >{props.state.name}</Text>
+      </View>
+      <View style={style}>
+        <PostMaker />
+      </View>
+    </ScrollView>
   );
 };
 
